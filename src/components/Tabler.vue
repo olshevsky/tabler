bbc2f9531b02def13d134a55fbc86d1aa0d554fb
artur <artur.olshevsky@gmail.com>
2018-05-03 20:54:21 +0300

    draft

--- src/components/Tabler.vue
+++ src/components/Tabler.vue
@@ -3,24 +3,36 @@
         <table>
             <thead>
             <tr>
-                <th v-for="field in fields">{{field.title}}</th>
+                <th v-for="field in fields">{{field.title | capitalize}}</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="row in visibleRows">
+            <tr v-for="row in paginatedRows">
                 <td v-for="field in fields">{{row[field.key]}}</td>
             </tr>
             </tbody>
         </table>
+
+        <br>
         <p>{{currentPage}} page from {{totalPages}}</p>
-        <button v-on:click="prevPage()">-</button>
-        <button v-on:click="nextPage()">+</button>
+        <button v-on:click="firstPage()"> &lt; &lt; </button>
+        <button v-on:click="prevPage()"> &lt; </button>
+        <input v-model="currentPage"/>
+        <button v-on:click="nextPage()"> &gt; </button>
+        <button v-on:click="lastPage()"> &gt; &gt; </button>
+
+
+        <br>
+        <p>Search by:</p>
         <input v-on:keyup="search()" v-model="searchBy"/>
 
     </div>
 </template>
 
 <script>
+
+//    let VueResource = require('vue-resource')
+
     module.exports = {
         props: {
             json: { type: Array },
@@ -38,7 +50,7 @@
         },
         methods: {
             fetchData: function(){
-                console.log('fetch data')
+                console.log('fetching data')
                 return []
             },
             nextPage: function () {
@@ -49,18 +61,20 @@
                 if(this.currentPage > 1)
                     this.currentPage--
             },
+            firstPage: function(){
+              this.currentPage = 1
+            },
+            lastPage: function () {
+              this.currentPage = this.totalPages
+            },
             search: function () {
                 console.log(this.searchBy)
-            },
-            sort: function () {
-                
             }
         },
         data: function () {
             return {
                 currentPage: this.page,
                 searchBy: null,
-                sortBy: null,
                 rawData: []
             }
         },
@@ -68,8 +82,16 @@
             rows: function () {
                 if(this.searchBy){
                     return this.rawData.filter(row => {
+                        for(let id in this.fields) {
+                            if(row[this.fields[id].key].toLowerCase().indexOf(this.searchBy) > 0){
+                                console.log('found')
+                                console.log(id)
+                            }
+                            //let a = row[this.fields[id].key].toLowerCase().includes(this.searchBy.toLowerCase())
+                        }
+
                         //post.title.toLowerCase().includes(this.search.toLowerCase())
-                        console.log(this.fields)
+//                        console.log(this.fields)
                         return row
                     })
                 }
@@ -77,7 +99,7 @@
                     return this.rawData
                 }
             },
-            visibleRows: function () {
+            paginatedRows: function () {
                 let startIndex = (this.currentPage - 1) * this.perPage
                 return this.rows.slice(startIndex, startIndex + this.perPage)
             },
@@ -86,18 +108,19 @@
             }
         },
         watch: {
-            'searchBy': function () {
-                console.log('searchBy change')
-                this.rows.filter(row => {
+            'currentPage': function () {
+              if(this.currentPage > this.totalPages)
+                  this.currentPage = this.totalPages
 
-                    return row
-                })
-//                let tmp =  this.rows.map((row)=>{
-//
-//                    console.log(by)
-//                    console.log(this.searchBy)
-//                    return row
-//                })
+              if(this.currentPage < 1)
+                  this.currentPage = 1
+            }
+        },
+        filters: {
+            capitalize: function (val) {
+                if(val)
+                    return val.toString().charAt(0).toUpperCase() + val.slice(1)
+                return ''
             }
         }
     }
