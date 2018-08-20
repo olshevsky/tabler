f73f7ec77b5b801104ea1721314ec78a4e27ec78
artur <artur.olshevsky@gmail.com>
2018-08-20 15:48:14 +0300

    cleanup

--- src/lib/fields.js
+++ src/lib/fields.js
@@ -66,7 +66,7 @@ export default{
                         result = (self.compare(field, filter) <  0) ? false : true
                         break
                     case '%like%':
-                        result = (field.value.indexOf(filter.value) > -1) ? true : false;
+                        result = (field.value.indexOf(filter.value) > -1) ? true : false
                         break
                     default:
                         result = false
@@ -86,23 +86,23 @@ export default{
         }
     },
     strToDate: function (val, format) {
-        let normalized = val.replace(/[^a-zA-Z0-9]/g, '-')
-        let normalizedFormat = format.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-')
-        let formatItems = normalizedFormat.split('-')
-        let dateItems = normalized.split('-')
-        let monthIndex  = formatItems.indexOf("mm")
-        let dayIndex    = formatItems.indexOf("dd")
-        let yearIndex   = formatItems.indexOf("yyyy")
-        let hourIndex     = formatItems.indexOf("hh")
-        let minutesIndex  = formatItems.indexOf("ii")
-        let secondsIndex  = formatItems.indexOf("ss")
-        let today = new Date()
-        let year  = yearIndex > - 1  ? dateItems[yearIndex]      : today.getFullYear()
-        let month = monthIndex > - 1 ? dateItems[monthIndex] - 1 : today.getMonth() - 1
-        let day   = dayIndex > - 1   ? dateItems[dayIndex]       : today.getDate()
-        let hour    = hourIndex > - 1      ? dateItems[hourIndex]    : today.getHours()
-        let minute  = minutesIndex > - 1   ? dateItems[minutesIndex] : today.getMinutes()
-        let second  = secondsIndex > - 1   ? dateItems[secondsIndex] : today.getSeconds()
+        let normalized = val.replace(/[^a-zA-Z0-9]/g, '-'),
+            normalizedFormat = format.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-'),
+            formatItems = normalizedFormat.split('-'),
+            dateItems = normalized.split('-'),
+            monthIndex  = formatItems.indexOf("mm"),
+            dayIndex    = formatItems.indexOf("dd"),
+            yearIndex   = formatItems.indexOf("yyyy"),
+            hourIndex     = formatItems.indexOf("hh"),
+            minutesIndex  = formatItems.indexOf("ii"),
+            secondsIndex  = formatItems.indexOf("ss"),
+            today = new Date(),
+            year  = yearIndex > - 1  ? dateItems[yearIndex]      : today.getFullYear(),
+            month = monthIndex > - 1 ? dateItems[monthIndex] - 1 : today.getMonth() - 1,
+            day   = dayIndex > - 1   ? dateItems[dayIndex]       : today.getDate(),
+            hour    = hourIndex > - 1      ? dateItems[hourIndex]    : today.getHours(),
+            minute  = minutesIndex > - 1   ? dateItems[minutesIndex] : today.getMinutes(),
+            second  = secondsIndex > - 1   ? dateItems[secondsIndex] : today.getSeconds()
         return new Date(year, month, day, hour, minute, second)
     }
 }
